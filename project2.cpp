894cd6202af2959e02a9f8847dccda7a917b2a72
Randy Pham <randypham76@yahoo.com>
2025-03-18 20:50:49 -0700

    Read in data file

--- project2.cpp
+++ project2.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
-
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <limits>
 
 using namespace std;
 
 
 
 
+// Function to read in data from file
+vector<vector<double>> loadData(const string& filename) {
+    vector<vector<double>> data;
+    ifstream file(filename);
+    string line;
 
+    while (getline(file, line)) {
+        stringstream ss(line);
+        vector<double> row;
+        double value;
+        while (ss >> value) {
+            row.push_back(value);
+        }
+        if (!row.empty()) {
+            data.push_back(row);
+        }
+    }
 
-
-int main()
-{
-    return 0;
+    file.close();
+    return data;
 }
 
+int main() {
+    string filename = "/Users/randypham/Downloads/CS170_Small_Data__97.txt"; 
+    vector<vector<double>> data = loadData(filename);
+
+
+    return 0;
+}
